ac70eaaa60c92f4505158d634876bd1b95027f4f
Gopal-nd <gopalhelpyou@gmail.com>
2025-04-17 11:52:46 +0530

    added landing

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
+
+app.get("/hi", (req, res) => {
+
+    res.send("HI!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server started on port 3000");
+});
